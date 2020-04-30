48c1a9372affe2861b9f6a324e8d9f90728e5a50
Remy L. Prechelt <rprechelt@gmail.com>
2020-04-29 16:16:41 -1000

    Corrected missing parent specification for fluxes.

--- python/PyFlux.cpp
+++ python/PyFlux.cpp
@@ -13,28 +13,28 @@ Py_Flux(py::module& m) {
   py::class_<Flux>(m, "Flux");
 
   // a fixed particle and energy for proton
-  py::class_<FixedParticleFlux<Proton>>(m, "FixedProtonFlux")
+  py::class_<FixedParticleFlux<Proton>, Flux>(m, "FixedProtonFlux")
     .def(py::init<const double>(),
          "Create a FixedProtonFlux at an energy in log10(eV).")
     .def("get_particle", &FixedParticleFlux<Proton>::get_particle,
          "Return a randomly sampled particle from this flux model.");
 
   // a fixed particle and energy for helium
-  py::class_<FixedParticleFlux<Helium>>(m, "FixedHeliumFlux")
+  py::class_<FixedParticleFlux<Helium>, Flux>(m, "FixedHeliumFlux")
     .def(py::init<const double>(),
          "Create a FixedHeliumFlux at an energy in log10(eV).")
     .def("get_particle", &FixedParticleFlux<Helium>::get_particle,
          "Return a randomly sampled particle from this flux model.");
 
   // a fixed particle and energy for nitrogen
-  py::class_<FixedParticleFlux<Nitrogen>>(m, "FixedNitrogenFlux")
+  py::class_<FixedParticleFlux<Nitrogen>, Flux>(m, "FixedNitrogenFlux")
     .def(py::init<const double>(),
          "Create a FixedNitrogenFlux at an energy in log10(eV).")
     .def("get_particle", &FixedParticleFlux<Nitrogen>::get_particle,
          "Return a randomly sampled particle from this flux model.");
 
   // a fixed particle and energy for iron
-  py::class_<FixedParticleFlux<Iron>>(m, "FixedIronFlux")
+  py::class_<FixedParticleFlux<Iron>, Flux>(m, "FixedIronFlux")
     .def(py::init<const double>(),
          "Create a FixedIronFlux at an energy in log10(eV).")
     .def("get_particle", &FixedParticleFlux<Iron>::get_particle,
